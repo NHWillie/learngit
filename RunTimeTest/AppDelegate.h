4ef226aeae6d4f5568b21da066bac3c808f7a7d3
wangwei <wangwei@wangwei.local>
2017-05-18 09:55:37 +0800

    Initial Commit

---
+++ RunTimeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunTimeTest
+//
+//  Created by wangwei on 2017/5/18.
+//  Copyright © 2017年 wangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
